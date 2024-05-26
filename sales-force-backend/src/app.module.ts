4ff13a46d2cc777799b6401c3c4a2073148fd795
JMSebas <jmoreira8505@uta.edu.ec>
2024-05-26 01:19:07 -0500

    Add Auth

--- sales-force-backend/src/app.module.ts
+++ sales-force-backend/src/app.module.ts
@@ -4,9 +4,12 @@ import { Module } from '@nestjs/common';
 // import { UserModule } from './user/user.module';
 // import { PrismaService } from './prisma/prisma.service';
 // import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+
 
 @Module({
-  imports: []
+  imports: [AuthModule, PrismaModule]
   // imports: [UserModule, PrismaModule],
   // controllers: [AppController],
   // providers: [AppService, PrismaService],
