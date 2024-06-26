fc9c4db9f355a4483c7b88248c09d25c6aea2ee2
vKail <ajurado5411@uta.edu.ec>
2024-06-25 20:59:29 -0500

    changes on delegations

--- sales-force-backend/src/features/delegation/application/delegation.service.ts
+++ sales-force-backend/src/features/delegation/application/delegation.service.ts
@@ -57,7 +57,36 @@ export class DelegationService implements DelegationInterfaceService{
       where:{
         id
       },
-      include: {employee: true, consumer: true}
+      include:{
+        employee: {
+          select: {
+            id: true,
+            username: true,
+            role: true,
+            person:  {
+                select:{
+                  dni: true,
+                  firstName: true,
+                  lastName: true
+                }
+              }
+            }
+          },
+        consumer: {
+          select: {
+            id: true,
+            isCustomer: true,
+            type: true,
+            person:  {
+                select:{
+                  dni: true,
+                  firstName: true,
+                  lastName: true
+                }
+              }
+            }
+          }     
+        }
     });
   }
 
